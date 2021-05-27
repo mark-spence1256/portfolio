d3c942984458b1d6a63f1d5cc269867c1c6aaf03
mark-spence1256 <markspence647@gmail.com>
2021-05-27 17:35:17 -0400

    master

--- my-app/src/component/SideNav.js
+++ my-app/src/component/SideNav.js
@@ -1,18 +1,28 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export const SideNav = () => {
     return (
         <div>
-           <ul className="sidenav">
-       <li><a href="https://www.linkedin.com/in/mark-spence-7a900617a/"><i class="fab fa-linkedin"></i></a></li>
-       <li><a href="https://github.com/mark-spence1256"><i class="fab fa-github"></i></a></li>
-       <li><a href="tel:647-572-1205"><i class="fas fa-phone"></i></a></li>
-       <li><a href="mailto:markspence647@gmail.com"><i class="fas fa-paper-plane"></i></a></li>
-       <li><a href="Mark Spence Web Developer .pdf"><i class="fas fa-file-pdf"></i></a></li>
+           <div className="sidenav">
+       <a href="https://www.linkedin.com/in/mark-spence-7a900617a/">
+           < FontAwesomeIcon icon={['fab', 'linkedin']} />
+           </a>
+       <a href="https://github.com/mark-spence1256">
+            < FontAwesomeIcon icon={['fab', 'github']} />
+       </a>
+       <a href="tel:6475721205">
+       < FontAwesomeIcon icon={['fa', 'phone']} />
+  </a>
+  <a href="mailto:markspence647@gmail.com">
+  < FontAwesomeIcon icon={['fa', 'paper-plane']} />
+</a>
+
+
        
  
-  </ul> 
+  </div> 
 
         </div>
     )
