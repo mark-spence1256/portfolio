6913c3da9d07fbe048c19b86fcf8d54b75d16042
mark-spence1256 <markspence647@gmail.com>
2021-05-27 18:28:31 -0400

    master

--- my-app/src/component/SideNav.js
+++ my-app/src/component/SideNav.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faPhone,faPaperPlane } from '@fortawesome/free-solid-svg-icons';
-import {fabGithub} from '@fortawesome/free-brands-svg-icons';
+import {fab} from '@fortawesome/free-brands-svg-icons';
 
 export const SideNav = () => {
     return (
         <div>
            <ul className="sidenav">
        <li><a href="https://www.linkedin.com/in/mark-spence-7a900617a/"><FontAwesomeIcon  style={{color:'red'}}/></a></li>
-       <li><a href="https://github.com/mark-spence1256"><FontAwesomeIcon icon={fabGithub} style={{color:'red'}} /></a></li>
+       <li><a href="https://github.com/mark-spence1256"><FontAwesomeIcon icon={["fab","github"]} style={{color:'red'}} /></a></li>
        <li><a href="tel:647-572-1205"><FontAwesomeIcon icon={faPhone} style={{color:'red'}} /></a></li>
        <li><a href="mailto:markspence647@gmail.com"><FontAwesomeIcon icon={faPaperPlane} style={{color:'red'}} /></a></li>
       
@@ -20,3 +20,4 @@ export const SideNav = () => {
     )
 }
 export default SideNav
+library.add(fab);
