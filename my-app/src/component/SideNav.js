cf297640c73584a0e27ee13acade49d229ffd2a2
mark-spence1256 <markspence647@gmail.com>
2021-05-27 17:56:31 -0400

    master

--- my-app/src/component/SideNav.js
+++ my-app/src/component/SideNav.js
@@ -10,8 +10,7 @@ export const SideNav = () => {
        <li><a href="tel:647-572-1205"><i className="fas fa-phone"></i></a></li>
        <li><a href="mailto:markspence647@gmail.com"><i className="fas fa-paper-plane"></i></a></li>
        <li><a href="Mark Spence Web Developer .pdf"><i className="fas fa-file-pdf"></i></a></li>
-       
- 
+    
   </ul> 
 
         </div>
