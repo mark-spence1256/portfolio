b6fb3bfc7f998ab6d9547ba85907aea6e437adc7
mark-spence1256 <markspence647@gmail.com>
2021-05-27 17:51:58 -0400

    master

--- my-app/src/component/SideNav.js
+++ my-app/src/component/SideNav.js
@@ -5,11 +5,11 @@ export const SideNav = () => {
     return (
         <div>
            <ul className="sidenav">
-       <li><a href="https://www.linkedin.com/in/mark-spence-7a900617a/"><i class="fab fa-linkedin"></i></a></li>
-       <li><a href="https://github.com/mark-spence1256"><i class="fab fa-github"></i></a></li>
-       <li><a href="tel:647-572-1205"><i class="fas fa-phone"></i></a></li>
-       <li><a href="mailto:markspence647@gmail.com"><i class="fas fa-paper-plane"></i></a></li>
-       <li><a href="Mark Spence Web Developer .pdf"><i class="fas fa-file-pdf"></i></a></li>
+       <li><a href="https://www.linkedin.com/in/mark-spence-7a900617a/"><i className="fab fa-linkedin"></i></a></li>
+       <li><a href="https://github.com/mark-spence1256"><i className="fab fa-github"></i></a></li>
+       <li><a href="tel:647-572-1205"><i className="fas fa-phone"></i></a></li>
+       <li><a href="mailto:markspence647@gmail.com"><i className="fas fa-paper-plane"></i></a></li>
+       <li><a href="Mark Spence Web Developer .pdf"><i className="fas fa-file-pdf"></i></a></li>
        
  
   </ul> 
