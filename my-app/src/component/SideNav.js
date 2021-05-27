2fa466f5482c1d97f1b49c282a285b1ead22c0b9
mark-spence1256 <markspence647@gmail.com>
2021-05-27 18:23:29 -0400

    master

--- my-app/src/component/SideNav.js
+++ my-app/src/component/SideNav.js
@@ -7,14 +7,14 @@ export const SideNav = () => {
     return (
         <div>
            <ul className="sidenav">
-       <li><a href="https://www.linkedin.com/in/mark-spence-7a900617a/"><FontAwesomeIcon icon={{faLinkedin}} style={{color:'red'}}/></a></li>
+       <li><a href="https://www.linkedin.com/in/mark-spence-7a900617a/"><FontAwesomeIcon  style={{color:'red'}}/></a></li>
        <li><a href="https://github.com/mark-spence1256"><i className="fab fa-github"></i></a></li>
        <li><a href="tel:647-572-1205"><FontAwesomeIcon icon={faPhone} style={{color:'red'}} /></a></li>
        <li><a href="mailto:markspence647@gmail.com"><i className="fas fa-paper-plane"></i></a></li>
       
     
   </ul> 
-  <FontAwesomeIcon icon={faThumbsUp} style={{color: 'red'}} />
+
 
         </div>
     )
