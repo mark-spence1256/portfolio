13ade91e8c9ef4fa479371e1b818985c890dba30
mark-spence1256 <markspence647@gmail.com>
2021-05-27 17:05:22 -0400

    master

--- my-app/src/component/Skills.js
+++ my-app/src/component/Skills.js
@@ -1,53 +1,32 @@
 import React from 'react'
 import {Component} from 'react'
-class  extends Component {
-    constructor(props) {
-        super(props)
-        this.myRef = React.createRef()
-        this.state = {
-          current: '',
-          count: 0,
-          index: 0,
-          letter: '',
-          arrow: false,
-          text: [
-            "Artist",
-            "photographer",
-            "Designer"
-          ],
-         
-        }
-     
-      }
-      componentDidMount = () => {
-     
-       this.type()
-      }
-      type = () => {
-        if(this.state.count === this.state.text.length) {
-            this.setState({count: 0})
-          } else {
-          this.setState({current: this.state.text[this.state.count],
-            index: this.state.index + 1,
-           letter: this.state.current.slice(0, this.state.index) 
-        
-          })
-          }  
-  
-        document.querySelector('.typing').textContent = this.state.letter
-         if(this.state.letter === this.state.current) {
-                this.setState({count: this.count + 1})
-                this.setState({index: 0})
-          }
-          setTimeout(this.type, 400) 
-     
-         
-        
+const textArray = ["HTML", "CSS","JAVASCRIPT", "REACT","VUE", "BOOTSTRAP","NODE", "PHP", "TAILWIND","LARAVEL", "MYSQL"];
 
-const Skills = () => {
+class Welcome extends Component {
+  constructor() {
+    super();
+    this.state = { textIdx: 0 };
+  }
+
+  componentDidMount() {
+    this.timeout = setInterval(() => {
+      let currentIdx = this.state.textIdx;
+      this.setState({ textIdx: currentIdx + 1 });
+    }, 1500);
+  }
+
+  componentDidUnmount() {
+    clearInterval(this.timeout);
+  }
+
+  render() {
+    let skills = textArray[this.state.textIdx % textArray.length];
+
+
     return (
         <div>
-            
+          <h1 style="color:white ">My skills:</h1>
+            <h1 style="color:red">{skills}</h1>
         </div>
     )
 }
