592f81c54d2887476ebcc094ab75085e94763715
mark-spence1256 <markspence647@gmail.com>
2021-06-23 13:50:28 -0400

    master

--- my-app/src/component/SideNav.js
+++ my-app/src/component/SideNav.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faPhone,faPaperPlane } from '@fortawesome/free-solid-svg-icons';
-import  {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import  {faGithub} from '@fortawesome/free-brands-svg-icons';
 
 export const SideNav = () => {
     return (
         <div>
            <ul className="sidenav">
-       <li><a href="https://www.linkedin.com/in/mark-spence-7a900617a/"><FontAwesomeIcon  icon={faLinkedin} style={{color:'red'}}/></a></li>
+      
        <li><a href="https://github.com/mark-spence1256"><FontAwesomeIcon icon={faGithub} style={{color:'red'}} /></a></li>
        <li><a href="tel:647-572-1205"><FontAwesomeIcon icon={faPhone} style={{color:'red'}} /></a></li>
        <li><a href="mailto:markspence647@gmail.com"><FontAwesomeIcon icon={faPaperPlane} style={{color:'red'}} /></a></li>
